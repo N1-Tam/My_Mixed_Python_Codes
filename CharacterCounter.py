8390123a5c5b0d20916b387c954d894e6cbe789a
N1_Tam <Kosti.playford407@schools.sa.edu.au>
2026-08-16 20:04:30 +0930

    Add files via upload

---
+++ CharacterCounter.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 16 19:53:14 2026
+
+@author: Konstantin.Playford
+"""
+
+word = input("Enter a word/phrase: ")
+count = (len(word))
+print ("There are", count, "letters in the word/phrase:", word)
