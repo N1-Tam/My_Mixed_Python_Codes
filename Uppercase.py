ca56da0a5a5a13e569980639802d666372ceeb26
N1_Tam <Kosti.playford407@schools.sa.edu.au>
2026-08-18 10:40:33 +0930

    Add files via upload

---
+++ Uppercase.py
@@ -0,0 +1,4 @@
+word = (input("Enter a word/phrase: "))
+choice = input("Would you like your word/s in capitals or lowercase? Enter your answer: ")
+if choice == ("capitals"): print (word.upper())
+elif choice == ("lowercase"):print (word.lower())
